391af4f198b97abed37509c3d0c052c259427059
RyanR06 <RyanRoelofs19032006@gmail.com>
2023-06-13 11:37:01 +0200

    Gun update
    reload and time between shots

--- Infected/Assets/scripts/Gun.cs
+++ Infected/Assets/scripts/Gun.cs
@@ -6,24 +6,97 @@ public class Gun : MonoBehaviour
 {
     public RaycastHit hit;
     public Camera cam;
-    public Enemy en;
+    public GameObject en;
     public float damage = 10f;
     public float delayFactor = 0.01f;
+    public bool hasShot;
+    public float ammo;
+    public bool canShoot;
+    public float maxAmmo = 30f;
 
 
 
 
     void Update()
     {
+        #region shoot
         if (Input.GetMouseButton(0))
         {
-            if (Physics.Raycast(cam.transform.position, transform.forward, out hit, 100))
+            if (canShoot == true)
             {
-                if (hit.transform.tag == "Enemies")
+                if (hasShot == false)
                 {
-                    en.health -= damage * delayFactor * Time.deltaTime;
+                    StopCoroutine(TBS());
+                    if (Physics.Raycast(cam.transform.position, transform.forward, out hit, 100))
+                    {
+                        if (hit.transform.tag == "Enemies")
+                        {
+                            en = hit.transform.gameObject;
+                            en.GetComponent<Enemy>().health -= damage;
+                            hasShot = true;
+                            StartCoroutine(TBS());
+                        }
+                    }
                 }
             }
+            
+            
         }
+        #endregion
+        if (ammo < 0)
+        {
+            canShoot = false;
+        }
+        else 
+            canShoot = true;
+        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+    
+    
+    
     }
+
+    public IEnumerator TBS()
+    {
+        if(hasShot == true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            hasShot = false;
+        }
+        
+    }
+
+    public IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(1f);
+        ammo = maxAmmo;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public void Start()
+    {
+        ammo = maxAmmo;
+    }
+
+
+
+
+
+
+
+
+
 }
